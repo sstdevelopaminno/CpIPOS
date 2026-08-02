082df821631fd2a7c3479a35c52655b006077a65
SST-CRUD <sstdevelopaminno@gmail.com>
2026-08-02 10:54:55 +0700

    Use hard serial reset after deploy changes

--- apps/backoffice-web/src/components/printing/browser-print-agent-deploy-reset.tsx
+++ apps/backoffice-web/src/components/printing/browser-print-agent-deploy-reset.tsx
@@ -4,10 +4,11 @@ import { useEffect } from "react";
 
 const BROWSER_PRINT_AGENT_CONFIG_EVENT = "cpi-browser-print-agent-config";
 const BROWSER_PRINT_AGENT_RESET_EVENT = "cpi-browser-print-agent-reset";
+const BROWSER_PRINT_AGENT_FORGET_PORTS_EVENT = "cpi-browser-print-agent-forget-ports";
 const BROWSER_PRINT_AGENT_ALERT_SNOOZE_UNTIL_KEY = "cpi_browser_print_agent_alert_snooze_until_v1";
 const BROWSER_PRINT_AGENT_DEPLOY_VERSION_KEY = "cpi_browser_print_agent_deploy_version_v1";
 const BUILD_INFO_URL = "/api/system/build-info";
-const FALLBACK_CLIENT_VERSION = "browser-print-agent-deploy-reset-2026-08-02";
+const FALLBACK_CLIENT_VERSION = "browser-print-agent-hard-serial-reset-2026-08-02";
 
 type BuildInfoResponse = {
   data?: {
@@ -17,8 +18,12 @@ type BuildInfoResponse = {
   } | null;
 };
 
-function dispatchAgentReset() {
+function dispatchAgentSoftReset() {
   window.dispatchEvent(new CustomEvent(BROWSER_PRINT_AGENT_RESET_EVENT));
+}
+
+function dispatchAgentHardReset() {
+  window.dispatchEvent(new CustomEvent(BROWSER_PRINT_AGENT_FORGET_PORTS_EVENT));
 }
 
 function dispatchAgentConfigReload() {
@@ -35,10 +40,11 @@ function applyDeployReset(version: string) {
 
   window.localStorage.setItem(BROWSER_PRINT_AGENT_DEPLOY_VERSION_KEY, version);
   clearTransientPrintAgentState();
-  dispatchAgentReset();
-  window.setTimeout(dispatchAgentConfigReload, 250);
-  window.setTimeout(dispatchAgentConfigReload, 1500);
-  window.setTimeout(dispatchAgentConfigReload, 5000);
+  dispatchAgentSoftReset();
+  window.setTimeout(dispatchAgentHardReset, 150);
+  window.setTimeout(dispatchAgentConfigReload, 750);
+  window.setTimeout(dispatchAgentConfigReload, 2500);
+  window.setTimeout(dispatchAgentConfigReload, 6000);
 }
 
 async function loadBuildVersion() {
