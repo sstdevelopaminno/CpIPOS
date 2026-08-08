3b776e3e720fd042bbd47b6ac9208f89d7ae551c
SST-CRUD <sstdevelopaminno@gmail.com>
2026-08-08 10:45:55 +0700

    Fix native Compose imports for Android build

--- apps/cpipos-mobile-android/app/src/main/java/com/cpipos/mobile/MobileApp.kt
+++ apps/cpipos-mobile-android/app/src/main/java/com/cpipos/mobile/MobileApp.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -43,7 +42,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
