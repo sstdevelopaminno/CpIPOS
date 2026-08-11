f756648dd7178b2786588d3e2c63cf1ee2af11b1
SST-CRUD <sstdevelopaminno@gmail.com>
2026-08-12 02:53:13 +0700

    feat(printers): connect MDM panel to printer settings

--- apps/backoffice-web/src/app/(backoffice)/backoffice/settings/printers/page.tsx
+++ apps/backoffice-web/src/app/(backoffice)/backoffice/settings/printers/page.tsx
@@ -1,5 +1,11 @@
 import { PrinterConnectionManagerV2 } from "@/components/backoffice/printer-connection-manager-v2";
+import { PrinterMdmPanel } from "@/components/backoffice/printer-mdm-panel";
 
 export default function BackofficePrintersSettingsPage() {
-  return <PrinterConnectionManagerV2 />;
+  return (
+    <>
+      <PrinterMdmPanel />
+      <PrinterConnectionManagerV2 />
+    </>
+  );
 }
