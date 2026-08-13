5c6fffe36c01f5fbcad930157be18b74956bd405
SST-CRUD <sstdevelopaminno@gmail.com>
2026-08-13 20:45:57 +0700

    feat(download): add Android POS 1.0.7 web page

---
+++ apps/backoffice-web/src/app/download/android-1-0-7/page.tsx
@@ -0,0 +1,10 @@
+import { DownloadCenterV107 } from "../download-center-v107";
+
+export const metadata = {
+  title: "Download CpIPOS Android POS 1.0.7",
+  description: "ดาวน์โหลด CpIPOS Android POS 1.0.7 Stable APK"
+};
+
+export default function AndroidPos107DownloadPage() {
+  return <DownloadCenterV107 />;
+}
