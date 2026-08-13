a089bab6346eb09b5afbeb4a2d80d1ac2ae9655c
SST-CRUD <sstdevelopaminno@gmail.com>
2026-08-13 23:45:39 +0700

    Publish Android POS 1.0.8 Download Center (#61)
    * feat(download): add Android POS 1.0.8 download center
    
    * feat(download): add Android POS 1.0.8 route
    
    * feat(download): switch download center to Android POS 1.0.8
    
    * fix(download): point Android POS 1.0.8 to signed release asset

---
+++ apps/backoffice-web/src/app/download/android-1-0-8/page.tsx
@@ -0,0 +1,10 @@
+import { DownloadCenterV108 } from "../download-center-v108";
+
+export const metadata = {
+  title: "Download CpIPOS Android POS 1.0.8",
+  description: "ดาวน์โหลด CpIPOS Android POS 1.0.8 Stable APK"
+};
+
+export default function AndroidPos108DownloadPage() {
+  return <DownloadCenterV108 />;
+}
